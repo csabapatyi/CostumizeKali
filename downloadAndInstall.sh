8ba9857bd4fc8c3042b21c9caa90690e89802f24
itoperatorguy <itoperatorguy@gmail.com>
2014-04-08 16:59:58 +0200

    rewrite the method how PATH set in downloadAndInstall.sh

--- downloadAndInstall.sh
+++ downloadAndInstall.sh
@@ -7,8 +7,6 @@ apt-get -y install git
 #create a directory for different additional tools what are installed later
 mkdir -p /root/extra_installed/
 cd /root/extra_installed/
-touch /root/extra_installed/addToPath.sh
-chmod u+x /root/extra_installed/addToPath.sh
 
 #download and install missing module for dnsennum
 #source: http://www.question-defense.com/2010/05/22/warning-cant-load-netwhoisip-module-whois-queries-desabled
@@ -22,12 +20,7 @@ make
 make install
 
 
-##Donwload subbrute
+##Donwload subbrute and add to PATH in bashrc file
 cd /root/extra_installed
 git clone https://github.com/TheRook/subbrute.git
-echo "export PATH=/root/extra_installed/subbrute/:$PATH" >> /root/extra_installed/addToPath.sh
-
-
-##Add external tools into PATH
-echo "/root/extra_installed/addToPath.sh" >> ~/.bashrc
-source ~/.bashrc
+echo "export PATH=/root/extra_installed/subbrute/:$PATH" >> ~/.bashrc
