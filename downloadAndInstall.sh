833e3e3561203aba97bde0fbc6dd43049d6de806
itoperatorguy <itoperatorguy@gmail.com>
2014-04-08 16:50:33 +0200

    add subbrute installation to downloadAndInstall.sh

--- downloadAndInstall.sh
+++ downloadAndInstall.sh
@@ -7,6 +7,8 @@ apt-get -y install git
 #create a directory for different additional tools what are installed later
 mkdir -p /root/extra_installed/
 cd /root/extra_installed/
+touch /root/extra_installed/addToPath.sh
+chmod u+x /root/extra_installed/addToPath.sh
 
 #download and install missing module for dnsennum
 #source: http://www.question-defense.com/2010/05/22/warning-cant-load-netwhoisip-module-whois-queries-desabled
@@ -20,4 +22,12 @@ make
 make install
 
 
+##Donwload subbrute
+cd /root/extra_installed
+git clone https://github.com/TheRook/subbrute.git
+echo "export PATH=/root/extra_installed/subbrute/:$PATH" >> /root/extra_installed/addToPath.sh
 
+
+##Add external tools into PATH
+echo "/root/extra_installed/addToPath.sh" >> ~/.bashrc
+source ~/.bashrc
